69fea4d742d8ac3829f4e6411096f69607eb5062
sshahaider <sshahaider@gmail.com>
2025-06-24 11:27:27 +0500

    feat (recent lyricslist)

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -3,14 +3,17 @@ import { CallToAction } from '@/components/home/call-to-action';
 import { BorderSeparator } from '@/components/sheard';
 import { Features } from '@/components/home/features';
 import { RecentLyrics } from '@/components/home/recent-lyrics';
+import { getLyricsData } from '@/lib/actions/lyrics';
 
-export default function Home() {
+export default async function Home() {
+	const { data: lyricsList } = await getLyricsData({ page: 1, take: 6 });
+
 	return (
 		<>
 			<Hero />
 			<BorderSeparator />
 			<Features />
-			<RecentLyrics />
+			<RecentLyrics lyricsList={lyricsList} />
 			<BorderSeparator />
 			<CallToAction />
 		</>
--- components/home/recent-lyrics.tsx
+++ components/home/recent-lyrics.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import { getLyricsData } from '@/lib/actions/lyrics';
 import { LyricsCard } from '../lyrics-card';
 import SectionTitle from '../sheard/section-title';
+import { CardType } from '@/types';
 
-export function RecentLyrics() {
+export function RecentLyrics({ lyricsList }: { lyricsList: CardType[] }) {
 	return (
 		<div className="w-full">
 			<SectionTitle text="Recent Lyrics" link="/lyrics" />
-			<RecentLyricsList />
-		</div>
-	);
-}
-
-export async function RecentLyricsList() {
-	const { data: lyricsList } = await getLyricsData({ page: 1, take: 6 });
-
-	return (
-		<div className="bp grid md:grid-cols-2 lg:grid-cols-3">
-			{lyricsList.map((lyric, index) => (
-				<LyricsCard key={index} data={lyric} />
-			))}
+			<div className="bp grid md:grid-cols-2 lg:grid-cols-3">
+				{lyricsList.map((lyric, index) => (
+					<LyricsCard key={index} data={lyric} />
+				))}
+			</div>
 		</div>
 	);
 }
