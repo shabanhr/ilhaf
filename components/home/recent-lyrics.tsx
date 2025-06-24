0256b115084771f043172c027223bd11f8de758e
sshahaider <sshahaider@gmail.com>
2025-06-24 09:56:53 +0500

    remove suspence from home page

--- components/home/recent-lyrics.tsx
+++ components/home/recent-lyrics.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import { getLyricsData } from '@/lib/actions/lyrics';
-import { LyricsCard, LyricsCardSkeleton } from '../lyrics-card';
+import { LyricsCard } from '../lyrics-card';
 import SectionTitle from '../sheard/section-title';
-
-export const dynamic = 'force-dynamic';
-
 
 export function RecentLyrics() {
 	return (
 		<div className="w-full">
 			<SectionTitle text="Recent Lyrics" link="/lyrics" />
-			<React.Suspense
-				fallback={
-					<div className="grid bp md:grid-cols-2 lg:grid-cols-3">
-						{Array.from({ length: 6 }).map((_, index) => (
-							<LyricsCardSkeleton key={index} />
-						))}
-					</div>
-				}
-			>
-				<RecentLyricsList />
-			</React.Suspense>
+			<RecentLyricsList />
 		</div>
 	);
 }
@@ -29,7 +16,7 @@ export async function RecentLyricsList() {
 	const { data: lyricsList } = await getLyricsData({ page: 1, take: 6 });
 
 	return (
-		<div className="grid bp md:grid-cols-2 lg:grid-cols-3">
+		<div className="bp grid md:grid-cols-2 lg:grid-cols-3">
 			{lyricsList.map((lyric, index) => (
 				<LyricsCard key={index} data={lyric} />
 			))}
