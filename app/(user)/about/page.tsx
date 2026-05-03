90931054cd66b240b9d7ab4c0f7cbe0b2f70fe75
shabanhr <sshahaider@gmail.com>
2026-05-03 22:48:40 +0500

    refactor(about): Remove commented-out code

--- app/(user)/about/page.tsx
+++ app/(user)/about/page.tsx
@@ -1,8 +1,6 @@
 import { getMetadata } from '@/lib/utils/metadata';
 import { BorderSeparator, PageDescription, PageHeading } from '@/components/sheard';
 import React from 'react';
-// import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-// import { drive } from '@/config';
 
 export const metadata = getMetadata({
 	title: 'About Us',
@@ -10,11 +8,6 @@ export const metadata = getMetadata({
 	url: `/about`,
 });
 
-// const members = [
-// 	{ image: 'shaban.webp', name: 'Shaban Haider', role: 'Founder ' },
-// 	{ image: 'abdullah.webp', name: 'Ali Abdullah', role: 'Content Writer' },
-// 	{ image: 'aoun.webp', name: 'Ali Aoun', role: 'Content Writer' },
-// ];
 
 export default function AboutPage() {
 	return (
@@ -31,26 +24,6 @@ export default function AboutPage() {
 					inspire and elevate the soul. Our website features an extensive library of lyrics, carefully curated to ensure
 					quality and authenticity.
 				</p>
-				{/* <div className="space-y-4">
-					<h2 className="text-xl font-semibold">Meet Our Team</h2>
-					<p className="text-muted-foreground">
-						The dedicated individuals behind the creation and curation of our ilhaf.com website.
-					</p>
-				</div>
-				<div className="grid gap-6 sm:grid-cols-2 md:gap-10 lg:grid-cols-3">
-					{members.map((member, index) => (
-						<div key={index} className="grid grid-cols-[auto_1fr] items-center gap-3">
-							<Avatar className="ring-foreground/10 size-10 rounded-(--radius) border border-transparent shadow ring-1">
-								<AvatarImage src={`${drive}/${member.image}`} alt={member.name} />
-								<AvatarFallback className="rounded-(--radius)">{member.name.charAt(0)}</AvatarFallback>
-							</Avatar>
-							<div>
-								<span className="text-foreground font-medium">{member.name}</span>
-								<div className="text-muted-foreground text-sm">{member.role}</div>
-							</div>
-						</div>
-					))}
-				</div> */}
 			</div>
 		</React.Fragment>
 	);
